b28356e218bd46f0b21eaf898e583de70eeafb5e
Naufal Pratomo <naufalpratomo8@gmail.com>
2024-02-16 15:59:50 +0700

    feat:fix interface

--- JobSheet1/pemilihan.java
+++ JobSheet1/pemilihan.java
@@ -6,15 +6,19 @@ public class pemilihan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Program Menghitung Nilai Akhir");
+        System.out.println("==============================");
         //input
-        System.out.println("Masukkan nilai tugas: ");
+        System.out.print("Masukkan nilai tugas: ");
         int tugas = sc.nextInt();
-        System.out.println("Masukkan nilai kuis: ");
+        System.out.print("Masukkan nilai kuis: ");
         int kuis = sc.nextInt();
-        System.out.println("Masukkan nilai UTS: ");
+        System.out.print("Masukkan nilai UTS: ");
         int uts = sc.nextInt();
-        System.out.println("Masukkan nilai UAS: ");
+        System.out.print("Masukkan nilai UAS: ");
         int uas = sc.nextInt();
+        System.out.println("==============================");
+        System.out.println("==============================");
 
         //batasan bila diluar ketentuan
         if (tugas < 0 || tugas > 100 || kuis < 0 || kuis > 100 || uts < 0 || uts > 100 || uas < 0 || uas > 100) {
@@ -45,13 +49,15 @@ public class pemilihan {
 
         if (nilaiHuruf.equals("A") || nilaiHuruf.equals("B+") || nilaiHuruf.equals("B") || nilaiHuruf.equals("C+")
                 || nilaiHuruf.equals("C")) {
-            keterangan = "LULUS";
+            keterangan = "SELAMAT ANDA LULUS";
         } else {
-            keterangan = "TIDAK LULUS";
+            keterangan = "MOHON MAAF ANDA TIDAK LULUS";
         }
 
         System.out.println("Nilai Akhir: " + nilaiAkhir);
         System.out.println("Nilai Huruf: " + nilaiHuruf);
-        System.out.println("Keterangan: " + keterangan);
+        System.out.println("==============================");
+        System.out.println("==============================");
+        System.out.println(keterangan);
     }
 }
