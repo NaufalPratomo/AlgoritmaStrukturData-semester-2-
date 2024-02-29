c39f8d83710445a1f26054150f63359588bca841
Naufal Pratomo <naufalpratomo8@gmail.com>
2024-02-29 09:49:16 +0700

    add: ArraySegitiga & Segitiga

---
+++ JobSheet3/Segitiga.java
@@ -0,0 +1,19 @@
+package JobSheet3;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga (int a, int t){
+        alas = a;
+        tinggi = t;
+    }
+
+    public int hitungLuas(){
+        return alas*tinggi/2;
+    }
+
+    public int hitungKeliling(){
+        return alas+tinggi+(int)Math.sqrt((alas*alas)+(tinggi*tinggi));
+    }
+}
