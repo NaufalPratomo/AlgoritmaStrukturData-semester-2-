ff7ff5083e755964627ca17bbc44c94e2b8f9d15
NaufalPratomo <naufalpratomo8@gmail.com>
2024-03-16 05:19:36 +0700

    add: Faktorial and MainFaktorial file

---
+++ BruteForceDivideConquer/minggu5/Faktorial.java
@@ -0,0 +1,23 @@
+package BruteForceDivideConquer.minggu5;
+
+public class Faktorial {
+
+    public int nilai;
+
+    int FaktorialBF(int n){
+        int fakto=1;
+        for(int i=1;i<=n;i++){
+            fakto=fakto*i;
+        }
+        return fakto;
+    }
+
+    int FaktorialDC(int n){
+        if(n==1){
+            return 1;
+        }else{
+            int fakto = n*FaktorialDC(n-1);
+            return fakto;
+        }
+    }
+}
---
+++ BruteForceDivideConquer/minggu5/MainFaktorial.java
@@ -0,0 +1,29 @@
+package BruteForceDivideConquer.minggu5;
+
+import java.util.Scanner;
+
+public class MainFaktorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-----------------");
+        System.out.println("Masukkan jumlah elemen: ");
+        int iJml = sc.nextInt();
+
+        Faktorial[] fk = new Faktorial[10];
+        for (int i = 0; i < iJml; i++) {
+            fk[i] = new Faktorial();
+            System.out.println("Masukkan nilai data ke-"+(i+1)+": ");
+            fk[i].nilai=sc.nextInt();
+        }
+
+        System.out.println("HASIL - DIVIDE AND CONQUER");
+        for (int i = 0; i < iJml; i++) {
+            System.out.println("Hasil Penghitungan faktorial menggunakan Brute Force adalah "+fk[i].FaktorialBF(fk[i].nilai));
+        }
+        System.out.println("Hasil _ DIVIDE AND CONQUER");
+        for (int i = 0; i < iJml; i++) {
+            System.out.println("Hasil Penghitungan faktorial menggunakan Divide and Conquer adalah "+fk[i].FaktorialDC(fk[i].nilai));
+        }
+    }
+}
