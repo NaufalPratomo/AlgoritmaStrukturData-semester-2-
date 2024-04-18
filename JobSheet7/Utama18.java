039ffa2bac55e48516737affa27a752352b79df0
NaufalPratomo <naufalpratomo8@gmail.com>
2024-04-18 20:49:01 +0700

    Modifikasi Pertanyaan

--- JobSheet7/Utama18.java
+++ JobSheet7/Utama18.java
@@ -1,28 +1,35 @@
 package JobSheet7;
 
+import java.util.Scanner;
+
 public class Utama18 {
     public static void main(String[] args) {
-        Gudang18 gudang = new Gudang18(7);
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas gudang: ");
+        int kapasitas = scanner.nextInt();
+        scanner.nextLine();
+
+        Gudang18 gudang = new Gudang18(kapasitas);
 
         while (true) {
             System.out.println("\nMenu");
             System.out.println("1. Tambah Barang");
             System.out.println("2. Ambil Barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. keluar");
-            System.out.println("Pilih operasi: ");
+            System.out.println("4. Lihat barang teratas");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih operasi: ");
             int pilihan = scanner.nextInt();
             scanner.nextLine();
 
             switch (pilihan) {
                 case 1:
-                    System.out.println("Masukkan kode barang: ");
+                    System.out.print("Masukkan kode barang: ");
                     int kode = scanner.nextInt();
                     scanner.nextLine();
-                    System.out.println("Masukkan nama barang: ");
+                    System.out.print("Masukkan nama barang: ");
                     String nama = scanner.nextLine();
-                    System.out.println("Masukkan nama kategori: ");
+                    System.out.print("Masukkan nama kategori: ");
                     String kategori = scanner.nextLine();
                     Barang18 barangBaru = new Barang18(kode, nama, kategori);
                     gudang.tambahBarang(barangBaru);
@@ -34,6 +41,9 @@ public class Utama18 {
                     gudang.tampilkanBarang();
                     break;
                 case 4:
+                    gudang.lihatBarangTeratas();
+                    break;
+                case 5:
                     break;
                 default:
                     System.out.println("Pilihan tidak valid, silahkan coba lagi.");
