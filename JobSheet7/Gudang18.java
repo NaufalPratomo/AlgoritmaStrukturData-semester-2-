6af7b4123b19445ee5ee2787ac3282af26406b06
NaufalPratomo <naufalpratomo8@gmail.com>
2024-04-18 21:22:13 +0700

    add: percobaan 2

--- JobSheet7/Gudang18.java
+++ JobSheet7/Gudang18.java
@@ -43,6 +43,7 @@ public class Gudang18 {
             Barang18 delete = tumpukan[top];
             top--;
             System.out.println("Barang " + delete.nama + " diambil dari Gudang");
+            System.out.println("Kode unik dalam biner: "+konversiDesimalKeBiner(delete.kode));
             return delete;
         } else {
             System.out.println("Tumpukan barang kosong.");
@@ -64,12 +65,27 @@ public class Gudang18 {
     public void tampilkanBarang() {
         if (!cekKosong()) {
             System.out.println("Rincian tumpukan barang di gudang:");
-            //for (int i = top; i >= 0; i--) {
+            // for (int i = top; i >= 0; i--) {
             for (int i = 0; i <= top; i++) {
-                System.out.printf("Kode %d: %s (kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama, tumpukan[i].kategori);
+                System.out.printf("Kode %d: %s (kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama,
+                        tumpukan[i].kategori);
             }
-            } else {
-                System.out.println("Tumpukan barang kosong");
-            }
+        } else {
+            System.out.println("Tumpukan barang kosong");
+        }
+    }
+
+    public String konversiDesimalKeBiner(int kode) {
+        StackKonversi stack = new StackKonversi();
+        while (kode > 0) {
+            int sisa = kode % 2;
+            stack.push(sisa);
+            kode = kode / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
     }
 }
